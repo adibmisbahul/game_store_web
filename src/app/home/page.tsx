5d85fe3bd8eb67857e64a2e09f8856aa69a1a65c
adibmisbahul <adibmisbahul101812@gmail.com>
2024-01-19 21:33:28 +0700

    lolo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,34 @@
+import Image from "next/image";
+
 import "./home.css";
 
 import SideBar from "../component/sidebar";
 import Navbar from "../component/navbar";
+import karakter from "../assets/karakter.png";
 
 export default function Home() {
   return (
     <div className="homesa">
       <div className="sideleftHome">
-        <SideBar/>
+        <SideBar />
       </div>
       <div className="siderightHome">
-        <Navbar/>
+        <Navbar />
         <div className="maincontent">
-          <div className="leftcontent"></div>
-          <div className="rightcontent"></div>
+          <div className="leftcontent">
+            <h1></h1>
+            <p></p>
+            <button>Check Now</button>  
+          </div>
+          <div className="rightcontent">
+            <Image
+              src={karakter}
+              width={400}
+              height={400}
+              alt=""
+              className="karakter"
+            />
+          </div>
         </div>
       </div>
     </div>
